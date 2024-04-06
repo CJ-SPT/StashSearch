71bf2f2a95bbaf7f131b090b66c20a308919d528
Michael P. Starkweather <git@mpstark.com>
2024-04-06 16:05:24 -0500

    fix issue with quick moving items during search

---
+++ StashSearch/Patches/CanQuickMoveToPatch.cs
@@ -0,0 +1,37 @@
+﻿using Aki.Reflection.Patching;
+using EFT.InventoryLogic;
+using HarmonyLib;
+using System.Reflection;
+
+namespace StashSearch.Patches
+{
+    internal class CanQuickMoveToPatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(ItemContextAbstractClass), nameof(ItemContextAbstractClass.CanQuickMoveTo));
+        }
+
+        /// <summary>
+        /// Prevents item loss by not allowing quick moves to stash while searching
+        /// </summary>
+        /// <returns></returns>
+        [PatchPostfix]
+        public static void PatchPostfix(ETargetContainer targetContainer, ref bool __result)
+        {
+            if (!__result || (targetContainer != ETargetContainer.Stash))
+            {
+                return;
+            }
+
+            foreach (var controller in Plugin.SearchControllers)
+            {
+                if (controller.IsSearchedState)
+                {
+                    __result = false;
+                    return;
+                }
+            }
+        }
+    }
+}
