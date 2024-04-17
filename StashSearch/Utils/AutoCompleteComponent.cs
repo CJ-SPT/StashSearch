f5625438162e74cb21b9477ee2f9a98affe65acf
Michael P. Starkweather <git@mpstark.com>
2024-04-16 20:20:12 -0500

    allow for multi-search autocomplete, add docstrings, autocomplete refinements

--- StashSearch/Utils/AutoCompleteComponent.cs
+++ StashSearch/Utils/AutoCompleteComponent.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using BepInEx;
+using EFT.Hideout;
 using EFT.InventoryLogic;
 using TMPro;
 using UnityEngine;
@@ -11,8 +13,11 @@ namespace StashSearch.Utils
     public class AutoCompleteComponent : MonoBehaviour
     {
         private TMP_InputField _inputField;
-        private char[] _trimChars = [' ', ',', '.', '/', '\\'];
-        private char[] _ignoreCharacters = ['(', ')', '[', ']', '"', '\''];
+
+        private static readonly char[] TRIM_CHARS = [' ', ',', '.', '/', '\\'];
+        private static readonly char[] REMOVE_CHARS = ['(', ')', '[', ']', '"', '\''];
+        private static readonly int MIN_KEYWORD_LENGTH = 2;
+
         private string _lastSearch;
         private string _lastSuggested;
 
@@ -53,10 +58,16 @@ namespace StashSearch.Utils
                 return;
             }
 
+            // FIXME: try and find a good way to bomb out if current caret is not at the end
+            // PROBLEM: caret position is not reliable here
+
             _lastSearch = thisSearch;
 
+            // split on commas for multi-searches, only use the last of the split
+            var splitSearch = thisSearch.Split(',');
+
             // find any available autocomplete, bomb if none
-            var autoCompleteSuffix = FindAutoCompleteSuffix(thisSearch);
+            var autoCompleteSuffix = FindAutoCompleteSuffix(splitSearch.Last());
             if (autoCompleteSuffix.IsNullOrEmpty())
             {
                 return;
@@ -77,12 +88,19 @@ namespace StashSearch.Utils
                 _inputField.selectionFocusPosition = caretPos + autoCompleteSuffix.Length;
                 _inputField.ForceLabelUpdate();
             });
-
-            Plugin.Log.LogDebug($"search: {thisSearch} autocomplete: {autoCompleteSuffix}");
         }
 
         private string FindAutoCompleteSuffix(string searchPrefix)
         {
+            // remove any starting spaces
+            searchPrefix = searchPrefix.TrimStart(' ');
+
+            // don't suggest if prefix is nothing
+            if (searchPrefix.IsNullOrWhiteSpace())
+            {
+                return string.Empty;
+            }
+
             // find keyword that matches our prefix
             var matches = _searchKeywords.Where(keywordPair => keywordPair.Key.StartsWith(searchPrefix));
             if (matches.IsNullOrEmpty())
@@ -90,13 +108,19 @@ namespace StashSearch.Utils
                 return string.Empty;
             }
 
-            // get the match with the largest count
-            var bestMatch = matches.OrderByDescending(keywordPair => keywordPair.Value).First().Key;
+            // get the match with the largest count, then the shortest autocomplete
+            var bestMatch = matches.OrderByDescending(keywordPair => keywordPair.Value)
+                                   .ThenByDescending(keywordPair => keywordPair.Key.Length)
+                                   .First().Key;
 
             // remove the prefix to return the suffix
             return bestMatch.Remove(bestMatch.IndexOf(searchPrefix), searchPrefix.Length);
         }
 
+        /// <summary>
+        /// Adds a keyword to the autocomplete
+        /// </summary>
+        /// <param name="keyword">keyword to add</param>
         public void AddKeyword(string keyword)
         {
             if (!_searchKeywords.ContainsKey(keyword))
@@ -109,6 +133,10 @@ namespace StashSearch.Utils
             }
         }
 
+        /// <summary>
+        /// Adds multiple keywords to the autocomplete
+        /// </summary>
+        /// <param name="keywords">keywords to add</param>
         public void AddKeywords(IEnumerable<string> keywords)
         {
             foreach (var keyword in keywords)
@@ -117,15 +145,21 @@ namespace StashSearch.Utils
             }
         }
 
+        /// <summary>
+        /// Splits a string, cleans it up, and adds each component to the autocomplete as keywords.
+        /// Duplicate words are condensed to a single keyword count
+        /// </summary>
+        /// <param name="toBeSplit">string to split and add</param>
         public void SplitStringAndAddToKeywords(string toBeSplit)
         {
             string[] splitString = toBeSplit.Split(' ');
+            HashSet<string> keywords = new();
 
             foreach (var untrimmedKeyword in splitString)
             {
                 // trim the ends and then remove the ignore characters from the string
-                var keyword = untrimmedKeyword.Trim(_trimChars);
-                foreach (var ignoreCharacter in _ignoreCharacters)
+                var keyword = untrimmedKeyword.Trim(TRIM_CHARS);
+                foreach (var ignoreCharacter in REMOVE_CHARS)
                 {
                     if (keyword.Contains(ignoreCharacter))
                     {
@@ -133,22 +167,38 @@ namespace StashSearch.Utils
                     }
                 }
 
-                if (string.IsNullOrWhiteSpace(keyword))
+                // bomb out before adding empty or too short of keywords
+                if (string.IsNullOrWhiteSpace(keyword) || keyword.Length <= MIN_KEYWORD_LENGTH)
                 {
                     continue;
                 }
 
-                AddKeyword(keyword);
+                keywords.Add(keyword);
             }
+
+            AddKeywords(keywords);
         }
 
+        /// <summary>
+        /// Adds a EFT item to the keywords by splitting the short name and long name into individual keywords
+        /// Duplicate words are condensed to a single keyword count
+        /// </summary>
+        /// <param name="item"></param>
         public void AddItemToKeywords(Item item)
         {
-            // add all of the possible auto completes for this item
-            SplitStringAndAddToKeywords(item.LocalizedShortName().ToLower());
-            SplitStringAndAddToKeywords(item.LocalizedName().ToLower());
+            // add full names as keywords
+            AddKeyword(item.LocalizedName().ToLower());
+            AddKeyword(item.LocalizedShortName().ToLower());
+
+            // add split words
+            // add the short name and long name together to be split, this ensures that duplicate words don't count more
+            SplitStringAndAddToKeywords($"{item.LocalizedShortName().ToLower()} {item.LocalizedName().ToLower()}");
         }
 
+        /// <summary>
+        /// Adds all contained EFT items in EFT grid to keywords, recursive to LootItemClass items
+        /// </summary>
+        /// <param name="gridToParse">grid to add</param>
         public void AddGridToKeywords(StashGridClass gridToParse) 
         {
             try
@@ -174,6 +224,9 @@ namespace StashSearch.Utils
             }
         }
 
+        /// <summary>
+        /// Clears keywords from internal dictionary
+        /// </summary>
         public void ClearKeywords()
         {
             _searchKeywords.Clear();
